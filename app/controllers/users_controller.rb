94d713ec1d701c8ba2dd4d4269aea334d5a7d9f3
Tr1ckC0 <trick.co@yahoo.com>
2020-06-03 11:55:36 -0500

    added User#create

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,29 @@
 class UsersController < ApplicationController
+    before_action :find_user, only: [:update, :destroy]
+    
     def index
         users = User.all
         
         render json: users
     end
+
+    def create
+        user = User.new(user_params)
+        
+        if user.save
+            render json: user
+        else
+            render json: {"error": "sign in / sign up failed"}
+        end
+    end
+
+        private
+
+        def find_user
+            @user = User.find(params[:id])
+        end
+
+        def user_params
+            params.require(:user).permit(:name, :email, :password, :password_confirmation)
+        end
 end
