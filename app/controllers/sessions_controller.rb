0ba543aef9fb30d77ad4748c2354d875296ea4b1
Tr1ckC0 <trick.co@yahoo.com>
2020-06-04 13:10:54 -0500

    added sessions#index / NEEDS REFACTOR

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,10 +1,22 @@
 class SessionsController < ApplicationController
+  
+  def index
+    session[:user_id] = 3
+    user = session[:user_id] && User.find(session[:user_id])
+
+    if user
+      render json: user
+    else
+      render json: {"error": "No current user"}
+    end
+  end
+  
   def create
     user = User.find_by(email: login_params[:email]) || User.create(login_params)
 
     if user && user.authenticate(login_params[:password])
       session[:user_id] = user.id
-      render json: {"message": "You are successfully logged in"}
+      render json: {"message": "#{session[:user_id]}"}
     else
       render json: {"message": "Invalid Username or Password"}
     end
